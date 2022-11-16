32c75036148926e286e4f609289a99a3d26e18e9
emoquery <nazardem4@gmail.com>
2022-11-16 12:32:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes");
+
+yargs.version("1.1.0");
+
+yargs.command({
+  command: "add",
+  describe: "add a new note",
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.removeNote(argv.title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "listing all notes",
+  handler() {
+    notes.listNotes();
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "reading a note",
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.readNote(argv.title);
+  },
+});
+
+yargs.parse();
